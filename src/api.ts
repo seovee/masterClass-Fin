b754d9d13992b8c08a33de7b498db2c3e9510213
seovee <jinseob102@naver.com>
2023-07-20 16:48:51 +0900

    9.5 home screen part one

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "290a48e21094b16c970edc7c28d201de";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
