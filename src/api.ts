7d0762c07e5593938b5f10a5b89932a8d3195597
seovee <jinseob102@naver.com>
2023-11-06 20:28:57 +0900

    api 수정

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 const API_KEY = "290a48e21094b16c970edc7c28d201de";
-const BASE_PATH = "https://api.themoviedb.org/3/movie";
+const BASE_PATH = "https://api.themoviedb.org/3";
 
 interface IMovie {
   id: number;
@@ -21,19 +21,19 @@ export interface IGetMoviesResult {
 }
 
 export function getNowPlaying() {
-  return fetch(`${BASE_PATH}/now_playing?api_key=${API_KEY}`).then((response) =>
-    response.json()
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
   );
 }
 
 export function getTopRated() {
-  return fetch(`${BASE_PATH}/top_rated?api_key=${API_KEY}`).then((response) =>
-    response.json()
+  return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}`).then(
+    (response) => response.json()
   );
 }
 
 export function getUpcoming() {
-  return fetch(`${BASE_PATH}/upcoming?api_key=${API_KEY}`).then((response) =>
-    response.json()
+  return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}`).then(
+    (response) => response.json()
   );
 }
