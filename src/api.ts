da9dbdc313460aab7ba12e226b2c209dd2d2950c
seovee <jinseob102@naver.com>
2024-01-18 23:23:57 +0900

    api 인터페이스 추가, 장르별 fetch 추가

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,23 @@
+import { Types } from "./utils";
+
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_PATH = "https://api.themoviedb.org/3";
 
 interface IMovie {
-  id: number;
+  adult: boolean;
   backdrop_path: string;
-  poster_path: string;
-  title: string;
+  genre_ids: [number, number, number, number];
+  id: number;
+  original_language: string;
+  original_title: string;
   overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
 export interface IGetMoviesResult {
@@ -18,6 +29,20 @@ export interface IGetMoviesResult {
   results: IMovie[];
   total_pages: number;
   total_results: number;
+}
+
+// 장르별 영화 검색
+export function getMovies(type: Types) {
+  return fetch(`${BASE_PATH}/movie/${type}?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+// 영화 상세 검색
+export function getMovieDetail(movieId: any) {
+  return fetch(`${BASE_PATH}/movie/${movieId}?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
 }
 
 export function getNowPlaying() {
