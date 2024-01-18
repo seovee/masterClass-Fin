1409aeeacef02d13f1ed8807587b43865df6426f
seovee <jinseob102@naver.com>
2024-01-18 23:24:12 +0900

    enum Types 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
 export function makeImagePath(id: string, format?: string) {
   return `https://image.tmdb.org/t/p/${format ? format : "original"}${id}`;
 }
+
+export enum Types {
+  "now_playing" = "now_playing",
+  "popular" = "popular",
+  "top_rated" = "top_rated",
+  "upcoming" = "upcoming",
+}
